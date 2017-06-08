cb296ae868b9639b6cfc733bfbd8ccb5a5853e9a
Colin Richardson <colinrichardson@bigfork.co.uk>
2017-06-08 14:46:07 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html><html><head><meta charset="utf-8" />
+<title>UNFUNKED UNFUNKED UNFUNKED UNFUNKED UNFUNKED UNFUNKED</title>
+<meta name="description" content="UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX" />
+<meta name="robots" content="noodp" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<meta property="og:title" content="UNFUNKED" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://www.unfunked.org/" />
+<link rel="image_src" href="http://www.unfunked.org/icon.png" type="image/png" />
+<link rel="apple-touch-icon" href="http://www.unfunked.org/icon.png">
+<meta property="og:image" content="http://www.unfunked.org/icon.png" />
+<meta property="og:image:type" content="image/png" />
+<meta property="og:description" content="UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX" />
+<meta property="og:site_name" content="UNFUNKED" />
+<meta property="og:locale" content="en_GB" />
+<link rel="shortcut icon" href="favicon.ico" />
+<link rel="stylesheet" href="c/ss.css" type="text/css" />
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-1321166-1', 'auto');
+ga('send', 'pageview');
+</script>
+</head>
+<body id="b">
+<div class="idiot"><div class="idiot"><pre class="stare" id="u">UF
+DX</pre></div></div>
+<script type="text/javascript">
+function h() { return '#' + ('000000' + Math.random().toString(16).slice(2,8).toUpperCase()).slice(-6); }
+var	d = document,
+	b = d.getElementById('b'),
+	u = d.getElementById('u'),
+	x = setInterval(function() {
+		b.style.backgroundColor=h();
+		u.style.color=h();
+	}, 200);
+</script></body></html>
