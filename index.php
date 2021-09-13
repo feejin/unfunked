fae46f5575c52e00ee3e50f66c47c4ba64fbb31b
Colin Richardson <colinrichardson@bigfork.co.uk>
2021-09-13 19:05:22 +0100

    let's get (in)accessible

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-header('HTTP/1.1 301 Moved Permanently'); 
-header('Location:/u/break/); 
+header('HTTP/1.1 301 Moved Permanently');
+header('Location:/u/wcag/');
---
+++ u/wcag/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html><html><head><meta charset="utf-8" />
+<title>UNFUNKED+</title>
+<meta name="description" content="UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX" />
+<meta name="robots" content="noodp" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<meta property="og:title" content="UNFUNKED" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://www.unfunked.org/" />
+<link rel="image_src" href="http://www.unfunked.org/icon.png" type="image/png" />
+<link rel="apple-touch-icon" href="http://www.unfunked.org/icon.png">
+<meta property="og:image" content="http://www.unfunked.org/icon.png" />
+<meta property="og:image:type" content="image/png" />
+<meta property="og:description" content="UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX UFDX" />
+<meta property="og:site_name" content="UNFUNKED+" />
+<meta property="og:locale" content="en_GB" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="stylesheet" href="c/ss.css" type="text/css" />
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-1321166-1"></script>
+<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-1321166-1');</script>
+</head><body><div id="w">The internet is not for everyone</div><script>
+let w=document.querySelector('#w'),t="span",s=w.innerHTML.replace(' not','').split(''),l=s.length,o='';
+for(i=0;i<l;i++){ o+='<'+t+((s[i]==' ')?'>&nbsp;':' tabindex='+i+'>'+s[i])+'</'+t+'>'; }
+w.innerHTML=o;</script></body></html>
